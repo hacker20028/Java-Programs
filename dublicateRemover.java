31e4a8358e4f91d2e1951ddb2dccd0207ab3ce97
hacker20028 <aryan20028@gmail.com>
2022-06-06 00:32:40 +0530

    An attempt to fix

--- dublicateRemover.java
+++ dublicateRemover.java
@@ -1,33 +1,60 @@
+
 //Accept 10 int values from user and delete the duplicate values.
 import java.util.*;
+
 public class dublicateRemover {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10]; 
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int temp = 0;
-        System.out.println("Enter 10 int values");
-        while (i < 10) {
-            arr[i] = sc.nextInt();
-            i++;
+    static void unique_array(int[] my_array) {
+        System.out.println("Original Array : ");
+
+        for (int i = 0; i < my_array.length; i++) {
+            System.out.print(my_array[i] + "\t");
         }
-        for (i = 0; i < 10; i++) {
-            for (j = i + 1; j < 10; j++) {
-                if (arr[i] == arr[j]) {
-                    temp = arr[j];
-                    for (k = j; k < 9; k++) {
-                        arr[k] = arr[k + 1];
-                    }
-                    arr[9] = temp;
+
+        // Assuming all elements in input array are unique
+
+        int no_unique_elements = my_array.length;
+
+        // Comparing each element with all other elements
+
+        for (int i = 0; i < no_unique_elements; i++) {
+            for (int j = i + 1; j < no_unique_elements; j++) {
+                // If any two elements are found equal
+
+                if (my_array[i] == my_array[j]) {
+                    // Replace duplicate element with last unique element
+
+                    my_array[j] = my_array[no_unique_elements - 1];
+
+                    no_unique_elements--;
+
+                    j--;
                 }
             }
         }
-        System.out.println("After removing duplicate values: ");
-        for (i = 0; i < 10; i++) {
-            System.out.println(arr[i]);
+
+        // Copying only unique elements of my_array into array1
+
+        int[] array1 = Arrays.copyOf(my_array, no_unique_elements);
+
+        // Printing arrayWithoutDuplicates
+
+        System.out.println();
+
+        System.out.println("Array with unique values : ");
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + "\t");
         }
+
+        System.out.println();
+
+        System.out.println("---------------------------");
     }
-    
+
+    public static void main(String[] args) {
+        unique_array(new int[] { 0, 3, -2, 4, 3, 2 });
+
+        unique_array(new int[] { 10, 22, 10, 20, 11, 22 });
+
+    }
 }
